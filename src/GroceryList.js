4f265990448cd998af7ad7765d0df862bc7eb9ca
Jesse <testemail645654@NotReal0232.com>
2020-10-31 14:48:48 -0500

    Removed unnecessary file.

--- src/GroceryList.js
+++
@@ -1,7 +0,0 @@
-export const createGroceryList = (userName) => {
-    return db.collection('groceryLists')
-        .add({
-            created: firebase.firestore.FieldValue.serverTimestamp(),
-            users: [{ name: userName}]
-        });
-};
