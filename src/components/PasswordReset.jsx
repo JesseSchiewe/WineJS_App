5558d8cf705e901512e7a36ddd941d6b7774fbf9
Jesse <testemail645654@NotReal0232.com>
2020-11-01 09:14:50 -0600

    Fixed unnecessary import in password reset component.

--- src/components/PasswordReset.jsx
+++ src/components/PasswordReset.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { auth } from "../Firebase";
 //import { UserContext } from "../providers/UserProvider";
 import { Link } from "react-router-dom";
