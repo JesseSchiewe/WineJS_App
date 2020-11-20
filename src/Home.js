e623f1e776a19a33c9b5dfad5ef3971185ebc6fe
Jesse <testemail645654@NotReal0232.com>
2020-11-20 14:38:34 -0600

    Added notes to Home.

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,10 @@ export default function Home() {
                 <h2>You must be signed in to access the Review and Review Results pages.</h2>
                 <div className="standardText" >
                     After you sign in, you can access all of the features from the hamburger menu at the top left of the screen.
+                    <p/>
+                    The review section allows you to record your thoughts about a wine.
+                    It saves your notes and scores into a database that you can access from anywhere.
+                    This can help you remember spectacular bottles of wine.
                 </div>
                 <p/>
                 <h2>Click on a link below</h2>
